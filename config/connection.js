65e6864f701627c6a4d851308234046118504cc3
Keyann <wkeyann@gmail.com>
2022-03-01 15:21:08 -0800

    connection.js fix

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/socialnetworkdb', {
+mongoose.connect('mongodb://localhost:27017/socialnetworkDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
